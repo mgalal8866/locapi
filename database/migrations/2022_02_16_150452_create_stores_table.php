e30479152b852fd00695fb9bc441dfa443df561d
mgalal8866 <58759061+mgalal8866@users.noreply.github.com>
2022-02-17 10:02:38 +0200

    dsfsdf

---
+++ database/migrations/2022_02_16_150452_create_stores_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('phone');
+            $table->string('address');
+            $table->string('name');
+            $table->text('note');
+            $table->integer('department_id');
+            $table->integer('region_id');
+            $table->string('lat');
+            $table->string('long');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
